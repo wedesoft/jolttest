f75ac7dc7996c318ebbc8997bd8fe2adf8c92499
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-09-11 21:38:41 +0100

    Add thread pool

--- tumble.cc
+++ tumble.cc
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <cstdarg>
+#include <thread>
 #include <Jolt/Jolt.h>
 #include <Jolt/Core/Factory.h>
 #include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
 
 using namespace std;
 using namespace JPH;
 
 static void TraceImpl(const char *inFMT, ...)
 {
-	va_list list;
-	va_start(list, inFMT);
-	char buffer[1024];
-	vsnprintf(buffer, sizeof(buffer), inFMT, list);
-	va_end(list);
-	cerr << buffer << endl;
+  va_list list;
+  va_start(list, inFMT);
+  char buffer[1024];
+  vsnprintf(buffer, sizeof(buffer), inFMT, list);
+  va_end(list);
+  cerr << buffer << endl;
 }
 
 int main(void)
@@ -23,6 +27,9 @@ int main(void)
   Trace = TraceImpl;
   Factory::sInstance = new Factory();
   RegisterTypes();
+
+  TempAllocatorMalloc allocator;
+  JobSystemThreadPool job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1);
 
   UnregisterTypes();
   delete Factory::sInstance;
