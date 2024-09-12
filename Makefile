ef596db8b593a2c99fe7e48efcd51f62fe0488c4
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-09-12 21:35:45 +0100

    Initialize physics system

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CCFLAGS = -g -Wall -Werror -DJPH_PROFILE_ENABLED -DJPH_DEBUG_RENDERER -DJPH_OBJECT_STREAM $(shell pkg-config --cflags glfw3 glew)
+CCFLAGS = -g -fPIC -Wall -Werror -DNDEBUG -DJPH_PROFILE_ENABLED -DJPH_DEBUG_RENDERER -DJPH_OBJECT_STREAM $(shell pkg-config --cflags glfw3 glew)
 LDFLAGS = $(shell pkg-config --libs glfw3 glew) -lJolt
 
 all: tumble
--- tumble.cc
+++ tumble.cc
@@ -7,7 +7,9 @@
 #include <Jolt/Core/TempAllocator.h>
 #include <Jolt/Core/JobSystemThreadPool.h>
 #include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
 #include <Jolt/Physics/Collision/ObjectLayer.h>
+#include <Jolt/Physics/Collision/BroadPhase/BroadPhaseLayer.h>
 #include <Jolt/Physics/Collision/Shape/BoxShape.h>
 
 
@@ -24,6 +26,23 @@ static void TraceImpl(const char *inFMT, ...)
   cerr << buffer << endl;
 }
 
+#ifdef JPH_ENABLE_ASSERTS
+
+// Callback for asserts, connect this to your own assert handler if you have one
+static bool AssertFailedImpl(const char *inExpression, const char *inMessage, const char *inFile, uint inLine)
+{
+  cerr << inFile << ":" << inLine << ": (" << inExpression << ") " << (inMessage != nullptr? inMessage : "") << endl;
+  return true;
+};
+
+#endif
+
+
+namespace Layers
+{
+  static constexpr ObjectLayer MOVING = 0;
+};
+
 class ObjectLayerPairFilterImpl: public ObjectLayerPairFilter
 {
   public:
@@ -32,22 +51,59 @@ class ObjectLayerPairFilterImpl: public ObjectLayerPairFilter
     }
 };
 
+namespace BroadPhaseLayers
+{
+  static constexpr BroadPhaseLayer MOVING(0);
+};
+
+class BPLayerInterfaceImpl final: public BroadPhaseLayerInterface
+{
+  public:
+    virtual uint GetNumBroadPhaseLayers() const override {
+      return 1;
+    }
+
+    virtual BroadPhaseLayer GetBroadPhaseLayer(ObjectLayer inLayer) const override {
+      return BroadPhaseLayers::MOVING;
+    }
+
+#if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
+    virtual const char *GetBroadPhaseLayerName(BroadPhaseLayer inLayer) const override {
+      return "MOVING";
+    }
+#endif
+};
+
+class ObjectVsBroadPhaseLayerFilterImpl : public ObjectVsBroadPhaseLayerFilter
+{
+public:
+  virtual bool ShouldCollide(ObjectLayer inLayer1, BroadPhaseLayer inLayer2) const override {
+    return true;
+  }
+};
+
 int main(void)
 {
   RegisterDefaultAllocator();
   Trace = TraceImpl;
+  JPH_IF_ENABLE_ASSERTS(AssertFailed = AssertFailedImpl;)
   Factory::sInstance = new Factory();
   RegisterTypes();
 
   TempAllocatorMalloc allocator;
   JobSystemThreadPool job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1);
 
-  // const uint cMaxBodies = 1024;
-  // const uint cNumBodyMutexes = 0;
-  // const uint cMaxBodyPairs = 1024;
-  // const uint cMaxContactConstraints = 1024;
-
+  const uint cMaxBodies = 1024;
+  const uint cNumBodyMutexes = 0;
+  const uint cMaxBodyPairs = 1024;
+  const uint cMaxContactConstraints = 1024;
+  BPLayerInterfaceImpl broad_phase_layer_interface;
   ObjectLayerPairFilterImpl object_vs_object_layer_filter;
+  ObjectVsBroadPhaseLayerFilterImpl object_vs_broadphase_layer_filter;
+
+  PhysicsSystem physics_system;
+  physics_system.Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, broad_phase_layer_interface,
+                      object_vs_broadphase_layer_filter, object_vs_object_layer_filter);
 
   UnregisterTypes();
   delete Factory::sInstance;
