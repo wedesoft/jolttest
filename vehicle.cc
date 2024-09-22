4fa3de75b0fafa068052d0ad335b61476e916085
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-09-22 21:42:45 +0100

    Try to display vehicle body

--- vehicle.cc
+++ vehicle.cc
@@ -87,9 +87,176 @@ public:
   }
 };
 
+int width = 1280;
+int height = 720;
+
+const char *vertexSource = "#version 410 core\n\
+uniform float aspect;\n\
+uniform vec3 axes;\n\
+uniform vec3 translation;\n\
+uniform mat3 rotation;\n\
+in vec3 point;\n\
+in vec3 normal;\n\
+out vec3 n;\n\
+void main()\n\
+{\n\
+  n = rotation * normal;\n\
+  gl_Position = vec4((rotation * (point * axes) + translation) * vec3(1, aspect, 1), 1);\n\
+}";
+
+const char *fragmentSource = "#version 410 core\n\
+uniform vec3 light;\n\
+in vec3 n;\n\
+out vec3 fragColor;\n\
+void main()\n\
+{\n\
+  float ambient = 0.3;\n\
+  float diffuse = 0.7 * max(dot(light, n), 0);\n\
+  fragColor = vec3(1, 1, 1) * (ambient + diffuse);\n\
+}";
+
+// Vertex array data
+GLfloat vertices[] = {
+  // Front face
+  -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+   0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+   0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+  -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+  // Back face
+  -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+   0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+   0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+  -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+  // Left face
+  -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+  -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+  -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+  -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+
+  // Right face
+   0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+   0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+   0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+   0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+
+  // Top face
+  -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+   0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+   0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+  -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+
+  // Bottom face
+  -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+   0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+   0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+  -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f
+};
+
+unsigned int indices[] = {
+   0,  1,  2,  3,
+   4,  5,  6,  7,
+   8,  9, 10, 11,
+  12, 13, 14, 15,
+  16, 17, 18, 19,
+  20, 21, 22, 23
+};
+
+void handleCompileError(const char *step, GLuint shader)
+{
+  GLint result = GL_FALSE;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+  if (result == GL_FALSE) {
+    char buffer[1024];
+    glGetShaderInfoLog(shader, 1024, NULL, buffer);
+    if (buffer[0])
+      fprintf(stderr, "%s: %s\n", step, buffer);
+  };
+}
+
+void handleLinkError(const char *step, GLuint program)
+{
+  GLint result = GL_FALSE;
+  glGetProgramiv(program, GL_LINK_STATUS, &result);
+  if (result == GL_FALSE) {
+    char buffer[1024];
+    glGetProgramInfoLog(program, 1024, NULL, buffer);
+    if (buffer[0])
+      fprintf(stderr, "%s: %s\n", step, buffer);
+  };
+}
+
 int main(void)
 {
   glfwInit();
+  GLFWwindow *window = glfwCreateWindow(width, height, "Falling stack of boxes with Jolt Physics", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  glewInit();
+
+  glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
+  glViewport(0, 0, width, height);
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexSource, NULL);
+  glCompileShader(vertexShader);
+  handleCompileError("Vertex shader", vertexShader);
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+  glCompileShader(fragmentShader);
+  handleCompileError("Fragment shader", fragmentShader);
+
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+  glLinkProgram(program);
+  handleLinkError("Shader program", program);
+
+  GLuint vao;
+  GLuint vbo;
+  GLuint idx;
+
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glGenBuffers(1, &idx);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glUseProgram(program);
+
+  glVertexAttribPointer(glGetAttribLocation(program, "point"),
+                        3, GL_FLOAT, GL_FALSE,
+                        6 * sizeof(float), (void *)0);
+  glVertexAttribPointer(glGetAttribLocation(program, "normal"),
+                        3, GL_FLOAT, GL_FALSE,
+                        6 * sizeof(float), (void *)(3 * sizeof(float)));
+
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glDisable(GL_CULL_FACE);
+  glEnable(GL_DEPTH_TEST);
+
+	const float wheel_radius = 0.03f;
+	const float wheel_width = 0.02f;
+	const float half_vehicle_length = 0.15f;
+	const float half_vehicle_width = 0.1f;
+	const float half_vehicle_height = 0.02f;
+	const float max_steering_angle = DegreesToRadians(30.0f);
+
+  float light[3] = {0.36f, 0.8f, -0.48f};
+  glUniform3fv(glGetUniformLocation(program, "light"), 1, light);
+  glUniform1f(glGetUniformLocation(program, "aspect"), (float)width / (float)height);
+  float a = half_vehicle_width * 2.0f;;
+  float b = half_vehicle_height * 2.0f;
+  float c = half_vehicle_length * 2.0f;
+  float axes[3] = {a, b, c};
+  glUniform3fv(glGetUniformLocation(program, "axes"), 1, axes);
 
   RegisterDefaultAllocator();
   Trace = TraceImpl;
@@ -122,37 +289,31 @@ int main(void)
   BodyCreationSettings ground_settings(ground_shape, RVec3(0.0, -0.5, 0.0), Quat::sIdentity(), EMotionType::Static, Layers::MOVING);
   Body *ground = body_interface.CreateBody(ground_settings);
   ground->SetFriction(0.5);
+  ground->SetRestitution(0.3f);
   body_interface.AddBody(ground->GetID(), EActivation::DontActivate);
 
-	const float wheel_radius = 0.03f;
-	const float wheel_width = 0.02f;
-	const float half_vehicle_length = 0.15f;
-	const float half_vehicle_width = 0.1f;
-	const float half_vehicle_height = 0.02f;
-	const float max_steering_angle = DegreesToRadians(30.0f);
-
-	RefConst<Shape> car_shape = new BoxShape(Vec3(half_vehicle_length, half_vehicle_height, half_vehicle_width));
+	RefConst<Shape> car_shape = new BoxShape(Vec3(half_vehicle_width, half_vehicle_height, half_vehicle_length));
 	BodyCreationSettings car_body_settings(car_shape, RVec3::sZero(), Quat::sIdentity(), EMotionType::Dynamic, Layers::MOVING);
 	car_body_settings.mOverrideMassProperties = EOverrideMassProperties::CalculateInertia;
-	car_body_settings.mMassPropertiesOverride.mMass = 10.0f;
+	car_body_settings.mMassPropertiesOverride.mMass = 1500.0f;
 
 	VehicleConstraintSettings vehicle;
 
 	WheelSettingsWV *w1 = new WheelSettingsWV;
-	w1->mPosition = Vec3(half_vehicle_length - 2.0f * wheel_radius, -0.9f * half_vehicle_height, 0.0f);
+	w1->mPosition = Vec3(0.0f, -0.9f * half_vehicle_height, half_vehicle_length - 2.0f * wheel_radius);
 	w1->mMaxSteerAngle = max_steering_angle;
 	w1->mMaxHandBrakeTorque = 0.0f;
   w1->mRadius = wheel_radius;
   w1->mWidth = wheel_width;
 
 	WheelSettingsWV *w2 = new WheelSettingsWV;
-	w2->mPosition = Vec3(-half_vehicle_length + 2.0f * wheel_radius, -0.9f * half_vehicle_height, half_vehicle_width);
+	w2->mPosition = Vec3(half_vehicle_width, -0.9f * half_vehicle_height, -half_vehicle_length + 2.0f * wheel_radius);
 	w2->mMaxSteerAngle = 0.0f;
   w2->mRadius = wheel_radius;
   w2->mWidth = wheel_width;
 
 	WheelSettingsWV *w3 = new WheelSettingsWV;
-	w3->mPosition = Vec3(-half_vehicle_length + 2.0f * wheel_radius, -0.9f * half_vehicle_height, -half_vehicle_width);
+	w3->mPosition = Vec3(-half_vehicle_width, -0.9f * half_vehicle_height, -half_vehicle_length + 2.0f * wheel_radius);
 	w3->mMaxSteerAngle = 0.0f;
   w3->mRadius = wheel_radius;
   w3->mWidth = wheel_width;
@@ -164,22 +325,33 @@ int main(void)
 
   Body *car_body = body_interface.CreateBody(car_body_settings);
   body_interface.AddBody(car_body->GetID(), EActivation::Activate);
-  VehicleConstraint *c = new VehicleConstraint(*car_body, vehicle);
+  body_interface.SetLinearVelocity(car_body->GetID(), Vec3(0.0f, 0.0f, 0.0f));
+  VehicleConstraint *constraint = new VehicleConstraint(*car_body, vehicle);
   VehicleCollisionTester *tester = new VehicleCollisionTesterRay(Layers::MOVING);
-  c->SetVehicleCollisionTester(tester);
-  physics_system.AddConstraint(c);
-  physics_system.AddStepListener(c);
+  constraint->SetVehicleCollisionTester(tester);
+  physics_system.AddConstraint(constraint);
+  physics_system.AddStepListener(constraint);
 
-  WheeledVehicleController *vehicle_controller = static_cast<WheeledVehicleController *>(c->GetController());
+  WheeledVehicleController *vehicle_controller = static_cast<WheeledVehicleController *>(constraint->GetController());
   vehicle_controller->SetDriverInput(0.0f, 0.0f, 0.0f, 0.0f);
 
   double t = glfwGetTime();
-  while (true) {
+  while (!glfwWindowShouldClose(window)) {
     double dt = glfwGetTime() - t;
-    body_interface.ActivateBody(c->GetVehicleBody()->GetID());
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    body_interface.ActivateBody(constraint->GetVehicleBody()->GetID());
     RMat44 transform = body_interface.GetWorldTransform(car_body->GetID());
     RVec3 position = transform.GetTranslation();
-    cout << dt << " " << position.GetX() << " " << position.GetY() << " " << position.GetZ() << endl;
+    Vec3 x = transform.GetAxisX();
+    Vec3 y = transform.GetAxisY();
+    Vec3 z = transform.GetAxisZ();
+    float translation[3] = {(float)position.GetX(), (float)position.GetY(), (float)position.GetZ()};
+    glUniform3fv(glGetUniformLocation(program, "translation"), 1, translation);
+    float rotation[9] = {x.GetX(), y.GetX(), z.GetX(), x.GetY(), y.GetY(), z.GetY(), x.GetZ(), y.GetZ(), z.GetZ()};
+    glUniformMatrix3fv(glGetUniformLocation(program, "rotation"), 1, GL_TRUE, rotation);
+    glDrawElements(GL_QUADS, 24, GL_UNSIGNED_INT, (void *)0);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
     const int cCollisionSteps = 1;
     physics_system.Update(dt, cCollisionSteps, &temp_allocator, &job_system);
     t += dt;
@@ -192,6 +364,17 @@ int main(void)
   UnregisterTypes();
   delete Factory::sInstance;
   Factory::sInstance = nullptr;
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  glDeleteBuffers(1, &idx);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glDeleteBuffers(1, &vbo);
+  glBindVertexArray(0);
+  glDeleteVertexArrays(1, &vao);
+
+  glDeleteProgram(program);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
   glfwTerminate();
   return 0;
